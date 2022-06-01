643f0cae4dc61070b19af8a5684ccea34ce09d30
chobolack <98378211+chobolack@users.noreply.github.com>
2022-06-01 20:48:05 +0800

    Start介面

---
+++ Assets/Scripts/Plot/StartButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void Start()
+    {
+        SceneManager.LoadScene("Chapter1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("離開遊戲");  //Unity測試用
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
